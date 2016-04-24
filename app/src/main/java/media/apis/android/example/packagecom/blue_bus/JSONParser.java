e9a652c64714db31d1e43100517919e59f235e97
AliAliyev <aaliyev13@yahoo.com>
2016-04-24 05:32:55 +0100

    my rides nearly done (edit, delete functions)

--- app/src/main/java/media/apis/android/example/packagecom/blue_bus/JSONParser.java
+++ app/src/main/java/media/apis/android/example/packagecom/blue_bus/JSONParser.java
@@ -1,26 +1,36 @@
 package media.apis.android.example.packagecom.blue_bus;
 
-import android.app.ListActivity;
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Ali on 22/04/2016.
  */
-public class JSONParser extends ListActivity {
+public class JSONParser extends AppCompatActivity {
     String next;
     private ArrayList items = null;
-
+    private  static  final String RIDE_URL = "http://halfbloodprince.16mb.com/food.php";
+    UserSessionManager session;
+    ListView listView;
     /**
      * Called when the activity is first created.
      */
@@ -29,88 +39,99 @@ public class JSONParser extends ListActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
-
-
-final ArrayList list = new ArrayList();
-        list.add("alma");
-        list.add("armud");
-        list.add("heyva");
-        list.add("nar");
-        list.add("avokado");
-
-     //   next=null;
-        populate();
-//System.out.println(next);
-
-       // System.out.println(populate());
+        session = new UserSessionManager(getApplicationContext());
+        populate(session.getUserDetails().get(UserSessionManager.KEY_EMAIL));
+        listView = (ListView) findViewById(R.id.list);
     }
 
-    private  void populate() {
-
+    private  void populate(final String user) {
 
          class Test extends AsyncTask<String, Void, String> {
+             ProgressDialog loading;
              @Override
              protected void onPreExecute() {
                  super.onPreExecute();
-                 //loading = ProgressDialog.show(Register.this, "Please Wait", null, true, true);
+                 loading = ProgressDialog.show(JSONParser.this, "Please Wait", null, true, true);
              }
             @Override
-            protected String doInBackground(String[] params) {
+            protected String doInBackground(String... params) {
                 try {
-                    URL url = new URL ("http://halfbloodprince.16mb.com/food.php");
-                    HttpURLConnection urlConnection =
-                            (HttpURLConnection) url.openConnection();
-                    urlConnection.setRequestMethod("GET");
-                    urlConnection.setDoInput(true);
-                    urlConnection.setDoOutput(true);
-                    urlConnection.connect();
+
+                    //to fetch data from database
+                    HashMap<String,String> strData = new HashMap<>();
+                    strData.put("user", params[0]);
+
+                    URL url = new URL(RIDE_URL);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    con.setReadTimeout(15000);
+                    con.setConnectTimeout(15000);
+                    con.setRequestMethod("POST");
+                    con.setDoInput(true);
+                    con.setDoOutput(true);
+
+                    OutputStream os = con.getOutputStream();
+                    BufferedWriter writer = new BufferedWriter(
+                            new OutputStreamWriter(os, "UTF-8"));
+                    writer.write(getPostDataString(strData));
+                    writer.flush();
+                    writer.close();
+                    os.close();
 
                     BufferedReader bufferedReader =
                             new BufferedReader(new InputStreamReader(
-                                    urlConnection.getInputStream()));
+                                    con.getInputStream()));
                     next=bufferedReader.readLine();
-                 //   System.out.println(bufferedReader.readLine() + " text");
 
-
-                   // while ((next = bufferedReader.readLine()) != null) {
-
-                  //  }
                 }catch (Exception e) {
                     e.printStackTrace();
                 }
-                return "some message";
+                return " ";
             }
 
             @Override
             protected void onPostExecute(String s) {
                 super.onPostExecute(s);
+                loading.dismiss();
                 items = new ArrayList();
                 try {
                     JSONArray ja = new JSONArray(next);
-                    //   System.out.println(ja + "   jason");
+
                     for (int i = 0; i < ja.length(); i++) {
                         //  JSONObject jo = (JSONObject) ja.get(i);
-                        // items.add(jo.getString("food_name"));
+                        //  items.add(jo.getString("food_name"));
 
                         JSONObject jsonObj = ja.getJSONObject(i);
-                        items.add(jsonObj.getString("food_name"));
-                        // System.out.println(jsonObj.getString("food_name"));
-                        // System.out.println("\n\n");
-                        System.out.println(items);
+                        items.add(jsonObj.getString("start")+"\n"+">>"+"\n"+jsonObj.getString("destination")+
+                                                    "\n"+jsonObj.getString("depart_time")+"\n"+
+                                                     "Spare seats "+jsonObj.getString("seats"));
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
 
-                setListAdapter(new ArrayAdapter(
+                listView.setAdapter(new ArrayAdapter(
                         JSONParser.this, android.R.layout.simple_list_item_1,
                         items));
             }
          }
         Test test = new Test();
-        test.execute("http://halfbloodprince.16mb.com/food.php");
-        //System.out.println("aftafa");
-        //return items;
+        test.execute(user);
+    }
+
+    private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for(java.util.Map.Entry<String, String> entry : params.entrySet()){
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        }
+
+        return result.toString();
     }
 }
--- app/src/main/java/media/apis/android/example/packagecom/blue_bus/RidePublished.java
+++ app/src/main/java/media/apis/android/example/packagecom/blue_bus/RidePublished.java
@@ -1,14 +1,29 @@
 package media.apis.android.example.packagecom.blue_bus;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Ali on 20/04/2016.
  */
 public class RidePublished extends AppCompatActivity {
+
+    Button button;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ride_published);
+
+        button = (Button) findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Myrides.class));
+                finish();
+            }
+        });
     }
 }
